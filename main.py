cb5919a7986b2dc2cce52b9f26bc0f8fdf42bc18
rcpastasc <62120664+rcpastasc@users.noreply.github.com>
2021-12-12 17:10:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 class Asiento:
-	def __init__(self, color, precio, registro):
-		self.color = color
-		self.precio = precio
-		self.registro = registro
-
-	def cambiarColor(self, color):
-		if(color in ["rojo","verde","amarillo","negro","blanco"]):
-			self.color = color
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+        
+    def cambiarColor(self, color):
+        colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if (color == colores[0] or color == colores[1] or color == colores[2] or color == colores[3] or color == colores[4]): 
+            self.color = color    
+            
+            
 class Auto:
-	cantidadCreados = 0
-	def __init__(self, modelo, precio, asientos, marca, motor,registro):
-		self.modelo = modelo
-		self.precio = precio
-		self.asientos = asientos
-		self.marca = marca
-		self.motor = motor
-		self.registro = registro
-		
-	def cantidadAsientos(self):
-		i=0
-		for asiento in self.asientos:
-			if asiento != None:
-				i+=1
-		return 
-
-	def verificarIntegridad(self):
-		cadena = "Auto original"
-		for i in range(0, len(self.asientos)):
-			if(self.asientos[i] !=None):
-				if (self.registro != self.asientos[i].registro or self.registro != self.motor.registro or self.asientos[i].registro != self.motor.registro):
-					cadena = "Las piezas no son originales"
-			else:
-				pass
-
-		return cadena
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.registro = registro
+        self.motor = motor
+        
+    def cantidadAsientos(self):
+        numero_asientos = 0
+        
+        for i in range(0, len(self.asientos)):
+            if isinstance(self.asientos[i], Asiento):
+                numero_asientos += 1
+                
+        return numero_asientos
+    
+    def verificarIntegridad(self):
+        cadena = "Auto original"
+        
+        for i in range(0, len(self.asientos)):
+            if (self.asientos[i] != None):
+                if (self.registro != self.asientos[i].registro or self.registro != self.motor.registro or self.asientos[i].registro != self.motor.registro):
+                    cadena =  "Las piezas no son originales"          
+            else:
+                pass
+ 
+        return cadena
+    
 
 class Motor:
-	def __init__(self, numero, tipo, registro):
-		self.numero = numero
-		self.tipo = tipo
-		self.registro = registro
-	
-	def cambiarRegistro(self, registro):
-		self.registro = registro
-	def asignarTipo(self, tipo):
-		if(tipo in ["electrico", "gasolina"]):
-			self.tipo = tipo
-
-
-
-
-	
-
-
-		
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+        
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
